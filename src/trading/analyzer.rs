cae9b2f2eb1bf0786a0e1e49bd48f510087702c8
theghostmac <theghostmac@gmail.com>
2024-10-27 13:09:00 +0100

    [chore] pushing all i have locally

--- src/trading/analyzer.rs
+++ src/trading/analyzer.rs
@@ -1,6 +1,13 @@
 
 pub struct TransactionAnalyzer;
+use solana_transaction_status::UiTransactionStatusMeta;
 
 impl TransactionAnalyzer {
-    
+    pub fn analyze_transaction(
+        transaction: &UiTransactionStatusMeta,
+        wallet: &Pubkey,
+    ) -> Result<TransactionAnalysis, BotError> {
+        // TODO: implement properly.
+        let is_people `
+    }
 }
